41e2c0410dcf76c4b3504937e7f03fc55c601aa6
Justin Bozonier <darkxanthos@gmail.com>
2013-08-14 08:01:31 -0500

    Saving data in a separate folder and ignoring it for commits.

--- current_data.py
+++ current_data.py
@@ -5,6 +5,7 @@ import time
 
 while True:
 	current_file_name = datetime.datetime.now().strftime('%Y%m%d_%H%M') + '.json'
+	current_file_name = './DownloadedData/' + current_file_name
 	if not os.path.exists(current_file_name):
 		print "Downloading file... then sleeping."
 		urllib.urlretrieve ("http://divvybikes.com/stations/json", current_file_name)
