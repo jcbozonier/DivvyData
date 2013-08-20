39a9f480c92c63166a38572e37d33536fbacf85e
Justin Bozonier <darkxanthos@gmail.com>
2013-08-20 18:44:40 -0500

    adding sample data and also added a file that tells the latest file that exists.

--- current_data.py
+++ current_data.py
@@ -5,10 +5,13 @@ import time
 
 while True:
 	current_file_name = datetime.datetime.now().strftime('%Y%m%d_%H%M') + '.json'
-	current_file_name = './DownloadedData/' + current_file_name
-	if not os.path.exists(current_file_name):
+	data_folder = './DownloadedData/'
+	current_file_path = data_folder + current_file_name
+	if not os.path.exists(current_file_path):
 		print "Downloading file... then sleeping."
-		urllib.urlretrieve ("http://divvybikes.com/stations/json", current_file_name)
+		urllib.urlretrieve ("http://divvybikes.com/stations/json", current_file_path)
+		f = open(data_folder + 'current_file.txt','w')
+		f.write(current_file_name)
 	else:
 		print "File already exists. Sleeping"
 	time.sleep(30)
