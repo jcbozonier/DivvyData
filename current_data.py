35ecfbe8f4c8d7b50b533e11bb4bfc1c21036781
Justin Bozonier <darkxanthos@gmail.com>
2013-09-05 15:36:54 -0500

    Added  exception handling

--- current_data.py
+++ current_data.py
@@ -7,11 +7,14 @@ while True:
 	current_file_name = datetime.datetime.now().strftime('%Y%m%d_%H%M') + '.json'
 	data_folder = './DownloadedData/'
 	current_file_path = data_folder + current_file_name
-	if not os.path.exists(current_file_path):
-		print "Downloading file... then sleeping."
-		urllib.urlretrieve ("http://divvybikes.com/stations/json", current_file_path)
-		f = open(data_folder + 'current_file.txt','w')
-		f.write(current_file_name)
-	else:
-		print "File already exists. Sleeping"
+	try:
+		if not os.path.exists(current_file_path):
+			print "Downloading file... then sleeping."
+			urllib.urlretrieve ("http://divvybikes.com/stations/json", current_file_path)
+			f = open(data_folder + 'current_file.txt','w')
+			f.write(current_file_name)
+		else:
+			print "File already exists. Sleeping"
+	except:
+		continue 
 	time.sleep(30)
