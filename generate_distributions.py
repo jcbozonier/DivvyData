f11f0f99200bb933b67275e0f30b962da2437b2f
Justin Bozonier <darkxanthos@gmail.com>
2013-10-15 19:49:25 -0500

    Changed to JSON format

--- generate_distributions.py
+++ generate_distributions.py
@@ -28,8 +28,8 @@ for file_path in glob.glob("andys_data/*.json"):
 		if not key in stations:
 			stations[key] = {
 				'id': station_id,
-				'available_docks': [0]*150,
-				'available_bikes': [0]*150
+				'available_docks': [0]*55,
+				'available_bikes': [0]*55
 			}
 		station_info = stations[key]
 		station_info['available_bikes'][event['availableBikes']] += 1
@@ -42,9 +42,9 @@ for key in stations.keys():
 	station_id = key[0]
 	hour = key[2]
 	if not station_id in available_bikes_by_station:
-		available_bikes_by_station[station_id] = [[1]*150 for i in range(0,24)]
+		available_bikes_by_station[station_id] = [[0]*55 for i in range(0,24)]
 	if not station_id in available_docks_by_station:
-		available_docks_by_station[station_id] = [[1]*150 for i in range(0,24)]
+		available_docks_by_station[station_id] = [[0]*55 for i in range(0,24)]
 
 	available_bikes_by_station[station_id][hour] = map(lambda le_tuple: le_tuple[0] + le_tuple[1], zip(available_bikes_by_station[station_id][hour], stations[key]['available_bikes']))
 	available_docks_by_station[station_id][hour] = map(lambda le_tuple: le_tuple[0] + le_tuple[1], zip(available_docks_by_station[station_id][hour], stations[key]['available_docks']))
@@ -56,21 +56,22 @@ for station_id in available_bikes_by_station.keys():
 	for hour in xrange(0,24):
 		bike_available_distribution = bike_available_hours[hour]
 		dock_available_distribution = dock_available_hours[hour]
-		hours_for_bikes_normalized = [(1.0*i)/sum(bike_available_distribution) for i in bike_available_distribution]
+		hours_for_bikes_normalized = [round((1.0*i)/sum(bike_available_distribution), 6) if sum(bike_available_distribution) != 0 else 0 for i in bike_available_distribution]
 		station_bikes_hour = [station_id, hour]
 		station_bikes_hour.extend(hours_for_bikes_normalized)
 		station_bikes.append(station_bikes_hour)
-		hours_for_docks_normalized = [(1.0*i)/sum(dock_available_distribution) for i in dock_available_distribution]
+		hours_for_docks_normalized = [round((1.0*i)/sum(dock_available_distribution), 6) if sum(dock_available_distribution) != 0 else 0 for i in dock_available_distribution]
 		station_docks_hour = [station_id, hour]
 		station_docks_hour.extend(hours_for_docks_normalized)
 		station_docks.append(station_docks_hour)
 
-with open('hour_bike_distributions.csv', 'w') as f: 
+with open('bike_availability_by_station_hour.csv', 'w') as f: 
 	writer = csv.writer(f)
 	writer.writerows(station_bikes)
-with open('hour_dock_distributions.csv', 'w') as f: 
-	writer = csv.writer(f)
-	writer.writerows(station_docks)
+with open('bike_availability_by_station_hour.json', 'w') as f: 
+	json.dump(station_bikes, f)
+with open('dock_availability_by_station_hour.json', 'w') as f: 
+	json.dump(station_docks, f)
 
 #station_info_for_csv = [{'station_id': item[0][0], 'weekend': item[0][1], 'hour':item[0][2], 'available_bikes':item[1]['available_bikes'], 'available_docks':item[1]['available_docks']} for item in stations.items()]
 
